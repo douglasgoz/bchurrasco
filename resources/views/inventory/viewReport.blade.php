2ad5a40f66cf09b156db1b6782ebc6fc4d6fd5b8
Douglas Sales <douglas@peoplexperience.com>
2025-02-05 02:59:21 -0300

    Adicionado cores para diferenciar adição de estoque e venda

--- resources/views/inventory/viewReport.blade.php
+++ resources/views/inventory/viewReport.blade.php
@@ -1,5 +1,19 @@
 @include('main.header')
 
+<head>
+    <style>
+        .text-red {
+            color: red!important;
+            font-weight: bold!important;
+        }
+
+        .text-green {
+            color: green!important;
+            font-weight: bold!important;
+        }
+    </style>
+</head>
+
 <div class="col-12 text-center">
     <table class="table table-bordered">
         <thead>
@@ -21,10 +35,10 @@
                     <th>{{$result->product->name ?? ''}}</th>
                     <th>{{$result->supplier}}</th>
                     <th>{{$result->category}}</th>
-                    <th>{{$result->quantity}}</th>
-                    <th>{{$result->price / $result->quantity}}</th>
-                    <th>{{$result->price}}</th>
-                    <th>{{$result->tax}}</th>
+                    <th class="{{ $result->quantity <= 0 ? 'text-red' : 'text-green' }}">{{$result->quantity}}</th>
+                    <th>{{$result->quantity > 0 ? ($result->price / $result->quantity) : ''}}</th>
+                    <th>{{$result->quantity > 0 ? $result->price : ''}}</th>
+                    <th>{{$result->quantity > 0 ? $result->tax : ''}}</th>
                 </tr>
             @endforeach
     </table>
